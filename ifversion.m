5e7ecb1ccbf817e043ee08dc3e2b408108ed40a7
thrynae <64639801+thrynae@users.noreply.github.com>
2022-09-16 11:57:46 +0200

    Added R2022b

--- ifversion.m
+++ ifversion.m
@@ -5,18 +5,18 @@ function tf=ifversion(test,Rxxxxab,Oct_flag,Oct_test,Oct_ver)
 % name is used that is not in the dictionary.
 %
 % Syntax:
-% tf=ifversion(test,Rxxxxab)
-% tf=ifversion(test,Rxxxxab,'Octave',test_for_Octave,v_Octave)
+%   tf = ifversion(test,Rxxxxab)
+%   tf = ifversion(test,Rxxxxab,'Octave',test_for_Octave,v_Octave)
 %
-% Output:
-% tf       - If the current version satisfies the test this returns true.
-%            This works similar to verLessThan.
-%
-% Inputs:
-% Rxxxxab - Char array containing a release description (e.g. 'R13', 'R14SP2' or 'R2019a') or the
-%           numeric version.
-% test    - Char array containing a logical test. The interpretation of this is equivalent to
-%           eval([current test Rxxxxab]). For examples, see below.
+% Input/output arguments:
+% tf:
+%   If the current version satisfies the test this returns true. This works similar to verLessThan.
+% Rxxxxab:
+%   A char array containing a release description (e.g. 'R13', 'R14SP2' or 'R2019a') or the numeric
+%   version (e.g. 6.5, 7, or 9.6).
+% test:
+%   A char array containing a logical test. The interpretation of this is equivalent to
+%   eval([current test Rxxxxab]). For examples, see below.
 %
 % Examples:
 % ifversion('>=','R2009a') returns true when run on R2009a or later
@@ -32,8 +32,8 @@ function tf=ifversion(test,Rxxxxab,Oct_flag,Oct_test,Oct_ver)
 %
 %/%/%/%/%/%/%/%/%/%/%/%/%/%/%/%/%/%/%/%/%/%/%/%/%/%/%/%/%/%/%/%/%/%/%/%/%/%/%
 %|                                                                         |%
-%|  Version: 1.1.1                                                         |%
-%|  Date:    2022-03-14                                                    |%
+%|  Version: 1.1.2                                                         |%
+%|  Date:    2022-09-16                                                    |%
 %|  Author:  H.J. Wisselink                                                |%
 %|  Licence: CC by-nc-sa 4.0 ( creativecommons.org/licenses/by-nc-sa/4.0 ) |%
 %|  Email = 'h_j_wisselink*alumnus_utwente_nl';                            |%
@@ -46,24 +46,22 @@ function tf=ifversion(test,Rxxxxab,Oct_flag,Oct_test,Oct_ver)
 % Compatibility considerations:
 % - This is expected to work on all releases.
 
-%The decimal of the version numbers are padded with a 0 to make sure v7.10 is larger than v7.9.
-%This does mean that any numeric version input needs to be adapted. multiply by 100 and round to
-%remove the potential for float rounding errors.
-%Store in persistent for fast recall (don't use getpref, as that is slower than generating the
-%variables and makes updating this function harder).
+% The decimal of the version numbers are padded with a 0 to make sure v7.10 is larger than v7.9.
+% This does mean that any numeric version input needs to be adapted. multiply by 100 and round to
+% remove the potential for float rounding errors.
+% Store in persistent for fast recall (don't use getpref, as that is slower than generating the
+% variables and makes updating this function harder).
 persistent  v_num v_dict octave
 if isempty(v_num)
-    %test if Octave is used instead of Matlab
-    octave=exist('OCTAVE_VERSION', 'builtin');
+    % Test if Octave is used instead of Matlab.
+    octave = exist('OCTAVE_VERSION', 'builtin');
     
-    %get current version number
-    v_num=version;
-    ii=strfind(v_num,'.');if numel(ii)~=1,v_num(ii(2):end)='';ii=ii(1);end
-    v_num=[str2double(v_num(1:(ii-1))) str2double(v_num((ii+1):end))];
-    v_num=v_num(1)+v_num(2)/100;v_num=round(100*v_num);
+    % Get current version number. This code was suggested by Jan on this thread:
+    % https://mathworks.com/matlabcentral/answers/1671199#comment_2040389
+    v_num = [100, 1] * sscanf(version, '%d.%d', 2);
     
-    %get dictionary to use for ismember
-    v_dict={...
+    % Get dictionary to use for ismember.
+    v_dict = {...
         'R13' 605;'R13SP1' 605;'R13SP2' 605;'R14' 700;'R14SP1' 700;'R14SP2' 700;
         'R14SP3' 701;'R2006a' 702;'R2006b' 703;'R2007a' 704;'R2007b' 705;
         'R2008a' 706;'R2008b' 707;'R2009a' 708;'R2009b' 709;'R2010a' 710;
@@ -71,7 +69,7 @@ if isempty(v_num)
         'R2013a' 801;'R2013b' 802;'R2014a' 803;'R2014b' 804;'R2015a' 805;
         'R2015b' 806;'R2016a' 900;'R2016b' 901;'R2017a' 902;'R2017b' 903;
         'R2018a' 904;'R2018b' 905;'R2019a' 906;'R2019b' 907;'R2020a' 908;
-        'R2020b' 909;'R2021a' 910;'R2021b' 911;'R2022a' 912};
+        'R2020b' 909;'R2021a' 910;'R2021b' 911;'R2022a' 912;'R2022b' 913};
 end
 
 if octave
@@ -80,47 +78,47 @@ if octave
             ['No version test for Octave was provided.',char(10),...
             'This function might return an unexpected outcome.']) %#ok<CHARTEN>
         if isnumeric(Rxxxxab)
-            v=0.1*Rxxxxab+0.9*fix(Rxxxxab);v=round(100*v);
+            v = 0.1*Rxxxxab+0.9*fix(Rxxxxab);v = round(100*v);
         else
-            L=ismember(v_dict(:,1),Rxxxxab);
+            L = ismember(v_dict(:,1),Rxxxxab);
             if sum(L)~=1
                 warning('HJW:ifversion:NotInDict',...
                     'The requested version is not in the hard-coded list.')
-                tf=NaN;return
+                tf = NaN;return
             else
-                v=v_dict{L,2};
+                v = v_dict{L,2};
             end
         end
     elseif nargin==4
         % Undocumented shorthand syntax: skip the 'Octave' argument.
-        [test,v]=deal(Oct_flag,Oct_test);
+        [test,v] = deal(Oct_flag,Oct_test);
         % Convert 4.1 to 401.
-        v=0.1*v+0.9*fix(v);v=round(100*v);
+        v = 0.1*v+0.9*fix(v);v = round(100*v);
     else
-        [test,v]=deal(Oct_test,Oct_ver);
+        [test,v] = deal(Oct_test,Oct_ver);
         % Convert 4.1 to 401.
-        v=0.1*v+0.9*fix(v);v=round(100*v);
+        v = 0.1*v+0.9*fix(v);v = round(100*v);
     end
 else
     % Convert R notation to numeric and convert 9.1 to 901.
     if isnumeric(Rxxxxab)
-        v=0.1*Rxxxxab+0.9*fix(Rxxxxab);v=round(100*v);
+        v = 0.1*Rxxxxab+0.9*fix(Rxxxxab);v = round(100*v);
     else
-        L=ismember(v_dict(:,1),Rxxxxab);
+        L = ismember(v_dict(:,1),Rxxxxab);
         if sum(L)~=1
             warning('HJW:ifversion:NotInDict',...
-                'The requested version is not in the hard-coded list.')
-            tf=NaN;return
+                    'The requested version is not in the hard-coded list.')
+            tf = NaN;return
         else
-            v=v_dict{L,2};
+            v = v_dict{L,2};
         end
     end
 end
 switch test
-    case '==', tf= v_num == v;
-    case '<' , tf= v_num <  v;
-    case '<=', tf= v_num <= v;
-    case '>' , tf= v_num >  v;
-    case '>=', tf= v_num >= v;
+    case '==', tf = v_num == v;
+    case '<' , tf = v_num <  v;
+    case '<=', tf = v_num <= v;
+    case '>' , tf = v_num >  v;
+    case '>=', tf = v_num >= v;
 end
 end
